bb0eb3eda00e00b5e16dce2e7ba19500ea41d8e0
NAGUIBATEUR228 <132890678+NAGUIBATEUR228@users.noreply.github.com>
2023-09-22 20:13:55 +0300

    Update barcode_dm.py

--- barcode_dm.py
+++ barcode_dm.py
@@ -73,36 +73,6 @@ if need_to_BLAST:
 
     #BLAST command, searching not_matched sequences in database uref
 
-    os.system(f'blastn -query dark_matter/{directory}_dm.fasta -db dark_matter/uref -out dark_matter/out/{directory}_blastout.txt -evalue 0.001 -word_size 6 -strand plus -max_target_seqs 2 -outfmt \"6 qacc qlen sacc slen length nident evalue qstart qend sstart send\"')#-num_threads 8
-
-    print(str(datetime.now())+' BLASTed')
-    #subtracting -unknownseqname in outfile.
-    with open(f'{dmp}\\out\\{directory}_blastout.txt','r') as f:
-        old_data=f.read()
-    new_data = 'qacc\tqlen\tsacc\tslen\tlength\tnident\tevalue\tqstart\tqend\tsstart\tsend\n'+old_data.replace('-unknownseqname\t','\t')
-    with open(f'{dmp}\\out\\{directory}_blastout.txt','w') as f:
-        f.write(new_data)
-
-check=pd.read_csv(f'{dmp}\\out\\{directory}_blastout.txt',sep='\t')
-#qacc - read sequence, sacc - primer name in 'ud'. 1 primer in sequence is not adequate
-check=check[check.groupby(['qacc'])['sacc'].transform('count')>=2]
-#check table is split in two parts. one of them contains sequences with more than one alignments with one primer. it is needed to choose only one.
-mask=check.groupby(['qacc','sacc'])['sacc'].transform('count')!=1
-check1=check[~mask]
-check=check[mask]
-# print(check.groupby(['qacc'])['sacc'].value_counts())
-
-#function for checking if there are sequences with two alignments with one primer
-def ch(check):
-    a=set(check.groupby(['qacc'])['sacc'].value_counts().values)
-    print(a,max(a))
-    return max(a)!=1
-
-changed=True
-if changed:
-    if ch(check):
-        check=check[(check.groupby(['qacc','sacc'])['length'].transform('max'))==check['length']]
-    else:
         changed=False
 if changed:
     if ch(check):
@@ -115,14 +85,57 @@ if changed:
         ]
 
         values = [
-            check['send'].values,
-            -check['sstart'].values,
-            check['send'].values,
-            -check['sstart'].values
+            check['qend'].values,
+            -check['qstart'].values,
+            check['qend'].values,
+            -check['qstart'].values
         ]
         check['tof'] = np.select(conditions, values, default=None)
-        check=check[(check.groupby(['qacc','sacc'])['tof'].transform('max'))==check['tof']]
+        check=check[(check.groupby(['qacc','sacc'])['tof'].transform('min'))==check['tof']]
     else:
+        changed=False
+print(ch(check))
+check=pd.concat([check,check1],ignore_index=True)
+sacc=check['sacc'].values
+conditions = [
+    sacc == 'u1',
+    sacc == 'u2',
+    sacc == 'u2_rc',
+    sacc == 'u1_rc'
+]
+
+values = [
+    check['qend'].values,
+    check['qstart'].values - 1,
+    check['qend'].values,
+    check['qstart'].values - 1
+]
+check['ind'] = np.select(conditions, values, default=None)
+print(str(datetime.now())+' pivot')
+w=pd.pivot(data=check, values='ind', index='qacc',columns='sacc').reset_index()
+w = w.where((pd.notnull(w)), None)
+u=w.columns.values.tolist()
+if sum([x in u for x in ud.keys()])==4:
+    mask=(~pd.isna(w['u1']) & ~pd.isna(w['u2']) & (w['u1']<=w['u2'])) | (~pd.isna(w['u1_rc']) & ~pd.isna(w['u2_rc']) & (w['u2_rc']<=w['u1_rc']))
+elif 'u1' in u and 'u2' in u:
+    mask=(~pd.isna(w['u1']) & ~pd.isna(w['u2']) & (w['u1']<=w['u2']))
+elif 'u1_rc' in u and 'u2_rc' in u:
+    mask=(~pd.isna(w['u1_rc']) & ~pd.isna(w['u2_rc']) & (w['u2_rc']<=w['u1_rc']))
+    if 'u1' in u: w.drop(columns=['u1'],inplace=True)
+    if 'u2' in u: w.drop(columns=['u2'],inplace=True)
+else: 
+    import sys
+    print('data have no barcode')
+    sys.exit()
+w=w[mask]
+print(str(datetime.now())+' extraction')
+v=np.vectorize(lambda x,y,z:x[y:z])
+vecrc=np.vectorize(rc)
+w['barcode']=w['qacc']
+if 'u1' in u: 
+    w['barcode']=v(w['qacc'].values,w['u1'].values,w['u2'].values)
+if 'u1_rc' in u: 
+    w['barcode']=np.where(w['barcode'].values==w['qacc'].values,
         vecrc(v(w['qacc'].values,w['u2_rc'].values,w['u1_rc'].values)),
         w['barcode'].values)
 w.set_index('qacc', inplace=True)
