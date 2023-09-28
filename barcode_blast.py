7e2417946027b5e5c9ca279ef5c894aba5c2a9ce
NAGUIBATEUR228 <132890678+NAGUIBATEUR228@users.noreply.github.com>
2023-09-28 13:18:33 +0300

    Update barcode_blast.py

--- barcode_blast.py
+++ barcode_blast.py
@@ -4,34 +4,35 @@ import pandas as pd
 import re
 from datetime import datetime
 from sys import argv
-if len(argv)>1:
-    need_to_BLAST=bool(int(argv[1]))
+
+if len(argv) > 1:
+    need_to_BLAST = bool(int(argv[1]))
 else:
-    need_to_BLAST=True
+    need_to_BLAST = True
 
 print('directory with script: C:\\Users\\zokmi\\Desktop\\study\\coursework\\')
-directory=input('enter directory with data: ')
-path=f'C:\\Users\\zokmi\\Desktop\\study\\coursework\\{directory}\\'
-print(str(datetime.now())+' '+path)
-#import reference barcode table, made by R scirpt
-ref=pd.read_csv(f'{path}..\\reference.txt')
+directory = input('enter directory with data: ')
+path = f'C:\\Users\\zokmi\\Desktop\\study\\coursework\\{directory}\\'
+print(str(datetime.now()) + ' ' + path)
+# import reference barcode table, made by R scirpt
+ref = pd.read_csv(f'{path}..\\reference.txt')
 print(str(datetime.now()))
 print(ref.head(5))
 
-dirs=list()
-for i in os.listdir(path):#list of directory and file names
+dirs = list()
+for i in os.listdir(path):  # list of directory and file names
     if os.path.isdir(os.path.join(path, i)):
         dirs.append(i)
-print(str(datetime.now())+' '+str(dirs))#directories with fastq files
+print(str(datetime.now()) + ' ' + str(dirs))  # directories with fastq files
 if need_to_BLAST:
-    #making fasta file for BLAST database
-    f=open('./ref_db.fasta',"w")
+    # making fasta file for BLAST database
+    f = open('./ref_db.fasta', "w")
     for i in range(len(ref['UPTAG_seqs'])):
-        i1=ref['UPTAG_seqs'][i]
-        i2=i1+'-refseqname'
+        i1 = ref['UPTAG_seqs'][i]
+        i2 = i1 + '-refseqname'
         f.write(f'>{i2}\n{i1}\n')
     f.close()
-    #BLAST command, making nucleotide database 'ref'
+    # BLAST command, making nucleotide database 'ref'
     os.system('makeblastdb -in ./ref_db.fasta -dbtype nucl -out ref')
 
 sumblast = pd.DataFrame({
@@ -41,127 +42,131 @@ sumblastdm = pd.DataFrame({
     'exp': [], 'all_count': [], 'm_count': [], 'percentage': [], 'qual': []
 })
 
-barcodes=pd.Series(dtype=object)
+barcodes = pd.Series(dtype=object)
 for i in dirs:
-    p=path+i+'\\artem'
+    p = path + i + '\\artem'
     content = os.listdir(p)
     files = []
     for file in content:
-        if os.path.isfile(os.path.join(p, file)) and (file.endswith('not_matched.csv') or file.endswith('not_matched_dm.csv')):
+        if os.path.isfile(os.path.join(p, file)) and (
+                file.endswith('not_matched.csv') or file.endswith('not_matched_dm.csv')):
             files.append(file)
-    #parsing not_matched files in folders
+    # parsing not_matched files in folders
     for j in files:
-        nb=pd.read_csv(f'{p}\\{j}')#not_matched table
-        #print(len(nb[~nb['barcode'].isin(barcodes)]))
-        barcodes=pd.Series(pd.concat([barcodes,nb['barcode']],ignore_index=True).unique())
+        nb = pd.read_csv(f'{p}\\{j}')  # not_matched table
+        # print(len(nb[~nb['barcode'].isin(barcodes)]))
+        barcodes = pd.Series(pd.concat([barcodes, nb['barcode']], ignore_index=True).unique())
 
 print(len(barcodes))
 if need_to_BLAST:
-    f=open(f'{path}\\all.fasta',"w")#making fasta with BLAST queries
+    f = open(f'{path}\\all.fasta', "w")  # making fasta with BLAST queries
     for k in barcodes:
         f.write(f'>{k}-unknownseqname\n{k}\n')
     f.close()
     os.system(f'cd {path}')
-    print(str(datetime.now())+' '+f'{p}\\all.fasta')
-    #BLAST command, searching not_matched sequences in database ref. output file end with blastout3.txt. e-value 10, alignment initiating word size 6, search on the same strand, finds only the best hit. outfile contains different information about alignment in tsv format.
-    os.system(f'blastn -query {path}\\all.fasta -db ref -out {path}\\all_blastout.txt -evalue 10 -word_size 6 -strand plus -max_target_seqs 1 -max_hsps 1 -outfmt \"6 qacc qlen sacc slen length nident evalue qstart qend sstart send\" -num_threads 10')#-num_threads 8
-    #subtracting -unknownseqname in outfile.
-    with open(f'{path}\\all_blastout.txt','r') as f:
-        old_data=f.read()
-    new_data = 'qacc\tqlen\tsacc\tslen\tlength\tnident\tevalue\tqstart\tqend\tsstart\tsend\n'+old_data.replace('-unknownseqname\t','\t')
-    with open(f'{path}\\all_blastout.txt','w') as f:
+    print(str(datetime.now()) + ' ' + f'{p}\\all.fasta')
+    # BLAST command, searching not_matched sequences in database ref. output file end with blastout3.txt. e-value 10, alignment initiating word size 6, search on the same strand, finds only the best hit. outfile contains different information about alignment in tsv format.
+    os.system(
+        f'blastn -query {path}\\all.fasta -db ref -out {path}\\all_blastout.txt -evalue 10 -word_size 6 -strand plus -max_target_seqs 1 -max_hsps 1 -outfmt \"6 qacc qlen sacc slen length nident evalue qstart qend sstart send\" -num_threads 10')  # -num_threads 8
+    # subtracting -unknownseqname in outfile.
+    with open(f'{path}\\all_blastout.txt', 'r') as f:
+        old_data = f.read()
+    new_data = 'qacc\tqlen\tsacc\tslen\tlength\tnident\tevalue\tqstart\tqend\tsstart\tsend\n' + old_data.replace(
+        '-unknownseqname\t', '\t')
+    with open(f'{path}\\all_blastout.txt', 'w') as f:
         f.write(new_data)
-    with open(f'{path}\\all_blastout.txt','r') as f:
-        old_data=f.read()
-    new_data = old_data.replace('-refseqname\t','\t')
-    with open(f'{path}\\all_blastout.txt','w') as f:
+    with open(f'{path}\\all_blastout.txt', 'r') as f:
+        old_data = f.read()
+    new_data = old_data.replace('-refseqname\t', '\t')
+    with open(f'{path}\\all_blastout.txt', 'w') as f:
         f.write(new_data)
 
-check=pd.read_csv(f'{path}\\all_blastout.txt',sep='\t')
+check = pd.read_csv(f'{path}\\all_blastout.txt', sep='\t')
 print(check)
-#filtering distance between sequences
-check['dist']=(check['slen']+check['qlen']-check['length']-check['nident'])
-check=check[check['dist']<=2]
+# filtering distance between sequences
+check['dist'] = (check['slen'] + check['qlen'] - check['length'] - check['nident'])
+check = check[check['dist'] <= 2]
 print(len(check))
-print(str(datetime.now())+' BLASTed')
-print(str(datetime.now())+' adding reference and not_matched data')
+print(str(datetime.now()) + ' BLASTed')
+print(str(datetime.now()) + ' adding reference and not_matched data')
 
-barcode=pd.DataFrame({'barcode':barcodes})
-full=pd.merge(barcode,check,left_on='barcode',right_on='qacc',how='left')
-full=pd.merge(full,ref,left_on='sacc',right_on='UPTAG_seqs',how='left')
-full=full[~pd.isna(full['qacc'])].reset_index()[['barcode','Confirmed_deletion','UPTAG_seqs','UPTAG_notes']]
+barcode = pd.DataFrame({'barcode': barcodes})
+full = pd.merge(barcode, check, left_on='barcode', right_on='qacc', how='left')
+full = pd.merge(full, ref, left_on='sacc', right_on='UPTAG_seqs', how='left')
+full = full[~pd.isna(full['qacc'])].reset_index()[['barcode', 'Confirmed_deletion', 'UPTAG_seqs', 'UPTAG_notes']]
 print(full)
 
-
 for i in dirs:
-    p=path+i+'\\artem'
+    p = path + i + '\\artem'
     content = os.listdir(p)
     files = []
     for file in content:
         if os.path.isfile(os.path.join(p, file)) and file.endswith('not_matched.csv'):
             files.append(file)
-    #parsing not_matched files in folders
+    # parsing not_matched files in folders
     for j in files:
-        nb=pd.read_csv(f'{p}\\{j}')#not_matched table
-        name=re.split(r'_not_matched\.csv',j)[0]
-        f=pd.merge(full,nb,left_on='barcode',right_on='barcode',how='right')
-        m=f[~pd.isna(f['Confirmed_deletion'])].reset_index()[['barcode',    'Confirmed_deletion',   'UPTAG_seqs', 'UPTAG_notes',    'n',    'count',    'qual']]
-        #print(str(datetime.now()))
+        nb = pd.read_csv(f'{p}\\{j}')  # not_matched table
+        name = re.split(r'_not_matched\.csv', j)[0]
+        f = pd.merge(full, nb, left_on='barcode', right_on='barcode', how='right')
+        m = f[~pd.isna(f['Confirmed_deletion'])].reset_index()[
+            ['barcode', 'Confirmed_deletion', 'UPTAG_seqs', 'UPTAG_notes', 'n', 'count', 'qual']]
+        # print(str(datetime.now()))
         m = m.groupby('Confirmed_deletion').agg(
-                barcode= ('barcode',lambda x: '|'.join(x[~pd.isna(x)].unique())),
-                n=('n', 'sum'),
-                count=('count', 'sum'),
-                notes=('UPTAG_notes', lambda x: '|'.join(x[~pd.isna(x)].unique())),
-                original_barcode=('UPTAG_seqs',lambda x: '|'.join(x[~pd.isna(x)].unique()))).reset_index()
-       
-        m=m.sort_values (by = ['count'], ascending = [ False ])
-        print(str(datetime.now())+' '+f'{p}\\{name}_blasted.csv')
-        m.to_csv (f'{p}\\{name}_blasted.csv', index= False )
+            barcode=('barcode', lambda x: '|'.join(x[~pd.isna(x)].unique())),
+            n=('n', 'sum'),
+            count=('count', 'sum'),
+            notes=('UPTAG_notes', lambda x: '|'.join(x[~pd.isna(x)].unique())),
+            original_barcode=('UPTAG_seqs', lambda x: '|'.join(x[~pd.isna(x)].unique()))).reset_index()
 
-        sumblast.loc[ len(sumblast.index )] = [
-        name,
-        round(nb['count'].sum()),
-        round(m['count'].sum()),
-        round(m['count'].sum())/round(nb['count'].sum()),
-        nb['qual'].mean().item()
+        m = m.sort_values(by=['count'], ascending=[False])
+        print(str(datetime.now()) + ' ' + f'{p}\\{name}_blasted.csv')
+        m.to_csv(f'{p}\\{name}_blasted.csv', index=False)
+
+        sumblast.loc[len(sumblast.index)] = [
+            name,
+            round(nb['count'].sum()),
+            round(m['count'].sum()),
+            round(m['count'].sum()) / round(nb['count'].sum()),
+            nb['qual'].mean().item()
         ]
 
 print(str(datetime.now()))
-print(str(sumblast['percentage'].tolist())+' '+'DONE')
-sumblast.to_csv (f'{path}sumblast.csv', index= False )
+print(str(sumblast['percentage'].tolist()) + ' ' + 'DONE')
+sumblast.to_csv(f'{path}sumblast.csv', index=False)
 
 for i in dirs:
-    p=path+i+'\\artem'
+    p = path + i + '\\artem'
     content = os.listdir(p)
     files = []
     for file in content:
         if os.path.isfile(os.path.join(p, file)) and file.endswith('not_matched_dm.csv'):
             files.append(file)
-    #parsing not_matched files in folders
+    # parsing not_matched files in folders
     for j in files:
-        nb=pd.read_csv(f'{p}\\{j}')#not_matched table
-        name=re.split(r'_not_matched_dm\.csv',j)[0]
-        f=pd.merge(full,nb,left_on='barcode',right_on='barcode',how='right')
-        m=f[~pd.isna(f['Confirmed_deletion'])].reset_index()[['barcode',    'Confirmed_deletion',   'UPTAG_seqs', 'UPTAG_notes',    'n',    'count',    'qual']]
+        nb = pd.read_csv(f'{p}\\{j}')  # not_matched table
+        name = re.split(r'_not_matched_dm\.csv', j)[0]
+        f = pd.merge(full, nb, left_on='barcode', right_on='barcode', how='right')
+        m = f[~pd.isna(f['Confirmed_deletion'])].reset_index()[
+            ['barcode', 'Confirmed_deletion', 'UPTAG_seqs', 'UPTAG_notes', 'n', 'count', 'qual']]
         m = m.groupby('Confirmed_deletion').agg(
-                barcode= ('barcode',lambda x: '|'.join(x[~pd.isna(x)].unique())),
-                n=('n', 'sum'),
-                count=('count', 'sum'),
-                notes=('UPTAG_notes', lambda x: '|'.join(x[~pd.isna(x)].unique())),
-                original_barcode=('UPTAG_seqs',lambda x: '|'.join(x[~pd.isna(x)].unique()))).reset_index()
-   
-        m=m.sort_values (by = ['count'], ascending = [ False ])
-        print(str(datetime.now())+' '+f'{p}\\{name}_blasted_dm.csv')
-        m.to_csv (f'{p}\\{name}_blasted_dm.csv', index= False )
+            barcode=('barcode', lambda x: '|'.join(x[~pd.isna(x)].unique())),
+            n=('n', 'sum'),
+            count=('count', 'sum'),
+            notes=('UPTAG_notes', lambda x: '|'.join(x[~pd.isna(x)].unique())),
+            original_barcode=('UPTAG_seqs', lambda x: '|'.join(x[~pd.isna(x)].unique()))).reset_index()
 
-        sumblastdm.loc[ len(sumblastdm.index )] = [
-        name,
-        round(nb['count'].sum()),
-        round(m['count'].sum()),
-        round(m['count'].sum())/round(nb['count'].sum()),
-        nb['qual'].mean().item()
+        m = m.sort_values(by=['count'], ascending=[False])
+        print(str(datetime.now()) + ' ' + f'{p}\\{name}_blasted_dm.csv')
+        m.to_csv(f'{p}\\{name}_blasted_dm.csv', index=False)
+
+        sumblastdm.loc[len(sumblastdm.index)] = [
+            name,
+            round(nb['count'].sum()),
+            round(m['count'].sum()),
+            round(m['count'].sum()) / round(nb['count'].sum()),
+            nb['qual'].mean().item()
         ]
 
 print(str(datetime.now()))
-print(str(sumblastdm['percentage'].tolist())+' '+'DONE')
-sumblastdm.to_csv (f'{path}sumblastdm.csv', index= False )
+print(str(sumblastdm['percentage'].tolist()) + ' ' + 'DONE')
+sumblastdm.to_csv(f'{path}sumblastdm.csv', index=False)
